b15376bef1b6f9bfc304ee0a79317384ae497a4d
Ashutosh Pratap Singh <ashutoshpratap26@gmail.com>
2024-06-13 17:14:24 +0530

    container config done

---
+++ Basic_01/variable.js
@@ -0,0 +1,14 @@
+const accountId = 26512131;
+let accountEmail = "ashutoshpratap26@gmail.com"
+
+var accontPassword = "2584124"
+accountCity = "Bijnor"
+// accountState;
+accountState= "UP"
+// accontPassword=8526852;  // Not Allowed
+
+// console.log(accountId);
+console.table([accountId, accontPassword, accountEmail, accountCity, accountState]);
+/*
+Dont use var
+*/
