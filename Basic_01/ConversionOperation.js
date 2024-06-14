8f6b6916e8d9b6df8d5e0a23d3706fd99321e888
Ashutosh Pratap Singh <ashutoshpratap26@gmail.com>
2024-06-14 09:34:54 +0530

    done

---
+++ Basic_01/ConversionOperation.js
@@ -0,0 +1,31 @@
+let account = "23acsck"
+// console.log(typeof a);
+// let num = 23
+// let stringNumber = String (num)
+//  console.log(typeof stringNumber);
+// let valueInNumber = Number(account)
+// console.log( valueInNumber);
+
+// let loggedIn = "AShu"
+// let booleanIsLoggedIn= Boolean (loggedIn)
+// console.log(+booleanIsLoggedIn);
+
+
+let number = 55
+
+let stringNumber = String (number)
+
+// console.log(stringNumber);
+// console.log (typeof stringNumber);
+
+
+
+// OPERATIONS //
+
+// console.log(2*2);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2**3);
+// console.log(5/2.5);
+// console.log(5%2);
+// console.log(2==5);
